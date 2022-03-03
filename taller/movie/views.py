28fd961ca7819d23a5b8f04cc3cf8cd5d0a482d6
drew <andresalazar138@gmail.com>
2022-03-03 08:57:41 -0500

    Visualizacion de peliculas

--- taller/movie/views.py
+++ taller/movie/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', { 'name': 'Greg Lim' })
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    if searchTerm:
+        movies = movies.filter(title__icontains=searchTerm)
+    return render(request, 'home.html', { 'searchTerm': searchTerm, 'movies': movies })
+
+def about(request):
+    return HttpResponse('<h1>Welcome to about page</h1>')
